3913b1b80f9d505a9f2c22572fe63993b8fab320
heermasterlp <heermaster@163.com>
2016-04-20 16:31:19 +0800

    fix bug

--- src/com/um/util/BasedOnRulePredict.java
+++ src/com/um/util/BasedOnRulePredict.java
@@ -30,7 +30,7 @@ public class BasedOnRulePredict {
 		medicineSet.add("茅莓根");
 		medicineSet.add("红豆杉");
 		
-		//3. 气虚－－－太子参 or 党参, 白朮, 黄芪 ＋ 甘草
+		// 2. 气虚－－－ 太子参 or 党参, 白朮, 黄芪 ＋ 甘草
 		if (description.contains("气虚")) {
 			medicineSet.add("党参");
 			medicineSet.add("白术");
@@ -46,7 +46,7 @@ public class BasedOnRulePredict {
 			medicineSet.add("甘草");
 			medicineSet.add("山药");
 		}
-		// 5. 阴虚 ---- 北沙参 ＋ 麦冬
+		// 5. 气阴两虚 ---- 北沙参 ＋ 麦冬
 		if (description.contains("气阴两虚")) {
 			medicineSet.add("北沙参");
 			medicineSet.add("麦冬");
@@ -58,7 +58,7 @@ public class BasedOnRulePredict {
 		}
 		
 		// 6. 胸肋痛----延胡索，，，牛蒡子OR 木蝴蝶
-		if (description.contains("胸肋痛")) {
+		if (description.contains("胸肋痛（轻）") ||description.contains("胸肋痛（中）") ||description.contains("胸肋痛（重）")) {
 			medicineSet.add("延胡索");
 			Random random = new Random();
 			String[] medicines = {"牛蒡子","木蝴蝶"};
@@ -68,13 +68,13 @@ public class BasedOnRulePredict {
 			medicineSet.add(medicines[result]);
 		}
 		
-		// 7. 厌食 or 食欲减退or纳差--> 炒谷芽 ＋ 山楂
+		// 7. 厌食 or 食欲减退or纳差 ------ 炒谷芽 ＋ 山楂
 		if (description.contains("厌食")||description.contains("食欲减退") || description.contains("纳差")) {
 			medicineSet.add("炒稻芽");
 			medicineSet.add("山楂");
 		}
 		
-		// 8. 腹胀 ＋ 便秘 --  轻：厚朴 ＋ 枳壳 ；重： 厚朴 ＋ 生大黄
+		// 8. 腹胀 ＋ 便秘 --------  轻：厚朴 ＋ 枳壳 ；重： 厚朴 ＋ 生大黄
 		if (description.contains("便秘") && (description.contains("重") )) {
 			medicineSet.add("番泻叶");
 			
@@ -84,28 +84,36 @@ public class BasedOnRulePredict {
 			medicineSet.add("火麻仁");
 		}
 		// 9. 睡眠差 －－  酸枣仁 ＋ 磁石
-		if (description.contains("失眠") && (description.contains("中") || description.contains("重"))) {
+		if (description.contains("失眠") || description.contains("嗜睡") ) {
 			medicineSet.add("酸枣仁");
 			medicineSet.add("磁石");
 		}
 		// 10. 红血痰 －－－ 紫珠草 ＋ 三七
 		if (description.contains("红血痰")) {
 			medicineSet.add("紫珠草");
-			medicineSet.add("三七末");
+			medicineSet.add("三七片");
 		}
 		// 11. 口渴多饮 －－－ 石斛 ＋ 天仁粉
 		if (description.contains("口渴多饮")) {
-			medicineSet.add("石斛");
-			medicineSet.add("天仁粉");
+			medicineSet.add("有瓜石斛");
+			medicineSet.add("天花粉");
 		}
 		
-		// 12. 腹泻 泄泻 ----- 石榴皮，五味子，补骨脂， 淮山药
+		// 12. 腹泻 泄泻 ----- 石榴皮，五味子，补骨脂， 淮山药 五选一
 		if (description.contains("腹泻")) {
-			medicineSet.add("石榴皮");
-			medicineSet.add("五味子");
-			medicineSet.add("补骨脂");
-			medicineSet.add("山药");
-			medicineSet.add("葶苈子");
+			
+			Random random = new Random();
+			String[] medicines = {"石榴皮","五味子","补骨脂","山药","葶苈子"};
+			int low = 0;
+			int high = 5;
+			int result = random.nextInt(high - low) + low;
+			medicineSet.add(medicines[result]);
+			int result1= random.nextInt(high - low) + low;
+
+			while( result == result1){
+				result1= random.nextInt(high - low) + low;
+			}
+			medicineSet.add(medicines[result1]);
 		}
 		
 		// 13. 头晕
@@ -113,12 +121,20 @@ public class BasedOnRulePredict {
 			medicineSet.add("天麻");
 			medicineSet.add("磁石");
 		}
-		// 14. 乏力
+		// 14. 乏力  4选2
 		if (description.contains("气力差") || description.contains("气力特差")) {
-			medicineSet.add("黄芪");
-			medicineSet.add("黄精");
-			medicineSet.add("桑葚");
-			medicineSet.add("肉桂蓉");
+			int low = 0;
+			int high = 4;
+			Random random = new Random();
+			String[] medicines = {"黄芪","黄精","桑葚","肉桂蓉"};
+			int result = random.nextInt(high - low) + low;
+			medicineSet.add(medicines[result]);
+			int result1= random.nextInt(high - low) + low;
+
+			while( result == result1){
+				result1= random.nextInt(high - low) + low;
+			}
+			medicineSet.add(medicines[result1]);
 		}
 		
 		// 15. 咳嗽
